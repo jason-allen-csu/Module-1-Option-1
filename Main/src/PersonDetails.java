0ed7e224e9c410e3c30bc5b19a30b8c860f9ae47
jason-allen-csu <jason.allen@csuglobal.edu>
2024-07-16 21:33:45 -0400

    Add files via upload
    Module-1 Option#1
    First commit of the completed code.

---
+++ Main/src/PersonDetails.java
@@ -0,0 +1,18 @@
+public class PersonDetails {
+	public static void main(String[] args) {
+		
+		// Initializing Person Details
+		String FirstName 	 = "T.D";
+		String LastName  	 = "Flipper";
+		String StreetAddress = "347 Don Shula Dr Suite 102";
+		String City			 = "Miami Gardens";
+		String ZipCode		 = "33056";
+		
+		// Printing Person Details
+		System.out.println(FirstName);
+		System.out.println(LastName);
+		System.out.println(StreetAddress);
+		System.out.println(City);
+		System.out.println(ZipCode);
+	}
+}
